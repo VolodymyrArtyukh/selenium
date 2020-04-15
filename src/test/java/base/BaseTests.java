446ffe597d22a78ca442a5f99420a1a8ca1e7e3f
Volodymyr Artyukh <volodymyr.artyukh@gmail.com>
2020-04-16 00:14:23 +0200

    TAU [Selenium WebDriver with Java]

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,27 +1,42 @@
 package base;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.google.common.io.Files;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.EventReporter;
+import utils.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTests
 {
-    private WebDriver driver;
+    //private WebDriver driver;
+    private EventFiringWebDriver driver;
     protected HomePage homePage;
 
     @BeforeClass
     public void setUp()
     {
         System.setProperty("webdriver.chrome.driver","resources/chromedriver");
-        driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/");
+        driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
+        driver.register(new EventReporter( ));
+        goHome();
+        setCookie();
+    }
 
+    @BeforeMethod
+    public void goHome()
+    {
+        driver.get("https://the-internet.herokuapp.com/");
         homePage = new HomePage(driver);
     }
 
@@ -31,5 +46,45 @@ public class BaseTests
         driver.quit();
     }
 
+    @AfterMethod
+    public void recordFailTests(ITestResult testResult)
+    {
+        if (ITestResult.FAILURE == testResult.getStatus())
+        {
+            var camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try
+            {
+                Files.move(screenshot, new File("resources/screenshots/" + testResult.getName() + ".png"));
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    public WindowManager getWindowManager()
+    {
+        return new  WindowManager(driver);
+    }
+
+    private ChromeOptions getChromeOptions()
+    {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-infobars");
+        //options.setHeadless(true);
+        return options;
+    }
+
+    private void setCookie()
+    {
+        //creates cookie
+        Cookie cookie =  new Cookie.Builder("TAU", "123")
+                .domain("the-internet.herokuapp.com")
+                .build();
+
+        //set cookie
+        driver.manage().addCookie(cookie );
+    }
 }
